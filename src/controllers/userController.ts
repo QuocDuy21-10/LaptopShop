e7e5648b240c4d49aae4c2861f888adde959e201
QuocDuy21-10 <quocduy37na@gmail.com>
2025-06-21 20:34:12 +0700

    video 48

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -10,11 +10,9 @@ const getHomePage = async (req : Request, res : Response)=> {
 const getCreateUserPage = (req: Request, res: Response) => {
     return res.render('create-user');
 }
-const postCreateUserPage = (req: Request, res: Response) => {
+const postCreateUserPage = async(req: Request, res: Response) => {
     const { fullName, email, address } = req.body;
-    console.log("req body", req.body);
-    
-    handleCreateUser( fullName, email, address);
+    await handleCreateUser( fullName, email, address);
     return res.redirect('/');
 }
 export { getHomePage , getCreateUserPage, postCreateUserPage};
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,14 @@
 import  getConnection  from '../config/db';
-const handleCreateUser = (fullName: string, email: string, address: string) => {
-    console.log("insert a new user");
+const handleCreateUser = async(fullName: string, email: string, address: string) => {
+    const connection = await getConnection();
+    // create a new user
+    try{
+        const sql = 'INSERT INTO `users` (`name`, `email`, `address`) VALUES (?,?,?)';
+        const values = [fullName, email, address];
+        const [result] = await connection.execute(sql, values);
+    } catch (err) {
+        console.log(err);
+    }
 }
 const getAllUsers = async()=> {
     const connection = await getConnection();
